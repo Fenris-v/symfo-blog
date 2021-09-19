1723327e4e4057e91af5c734eaf50c057b10189f
fenris <Fenris-v>
2021-09-19 22:19:12 +0300

    create main page template

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './css/app.css';
+
+import $ from 'jquery';
+import 'bootstrap';
+import 'jquery-easing';
+import './js/scripts';
+import './js/dashboard';
